b960c92d340f0f0ef848a131675ea4c3740719cd
BrandonHansen <bhansen4@nd.edu>
2017-12-02 19:13:18 +0000

    song additions

--- src/handbellHelper/handbellHelper/views.py
+++ src/handbellHelper/handbellHelper/views.py
@@ -9,14 +9,15 @@ def app(request):
     if request.method == 'POST':
         button = request.POST.get('button', None)
         if button == 'play':
-            select = request.POST.get('select', None)
+            select = request.POST.get('select', None).replace(' ','_')
+            print select
             proc = Popen(['python', '/home/pi/capstone/src/main.py', '-play', select])
-            print proc
+            #print proc
         elif button == 'stop':
             f = open("./../write.csv","r")
             output = f.read()
             f.close()
             bunk = output
     with open("./../sheet/songs.txt", "r") as f:
-        songs = f.read().split('\n')
+        songs = f.read().replace('_',' ').split('\n')
     return render(request, 'forms.html', {'song_dicts':songs, 'bunk':bunk})
