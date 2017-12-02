a0d84c1ffd9a96ba2d8e8e0fa7d5ddeb81bfe49f
Matthew Fabian <mfabian1@nd.edu>
2017-12-02 16:39:47 -0500

    add static files

--- src/handbellHelper/handbellHelper/settings.py
+++ src/handbellHelper/handbellHelper/settings.py
@@ -10,11 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
-import os
+import os, sys
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+CURRENT_DIR = os.path.dirname(__file__).replace('\\','/')
+PROJECT_ROOT = os.path.abspath(os.path.join(CURRENT_DIR, os.pardir))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
@@ -124,3 +125,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
