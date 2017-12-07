09a0193de970d567f71f40d07e88a9077375c681
Matthew Fabian <mfabian1@nd.edu>
2017-12-07 17:19:40 -0500

    add CORS functionality to server

--- src/server.py
+++ src/server.py
@@ -2,6 +2,7 @@ import cherrypy
 import json
 
 
+
 @cherrypy.expose
 class MusicWebService(object):
 
@@ -11,9 +12,6 @@ class MusicWebService(object):
         self.dict = {'currentLine' : 0}
 
     def GET(self):
-        print "GET REQUEST"
-        for key in self.dict:
-            print "Key: " + key + ", Value: " + self.dict[key]
         return self.dict['currentLine']
 
     def POST(self):
@@ -23,23 +21,48 @@ class MusicWebService(object):
 
     def PUT(self):
         data = cherrypy.request.body.read()
-        print "Data from server" + str(data)
         self.dict['currentLine'] = str(data)
-        for key in self.dict:
-            print "Key: " + key + ", Value: " + self.dict[key]
 
     def DELETE(self):
         self.dict.pop('currentLine', None)
 
+class Options:
+    def OPTIONS(self, *args, **kwargs):
+            return ""
+
+def CORS():
+    cherrypy.response.headers["Access-Control-Allow-Origin"] = "*"
+    cherrypy.response.headers["Access-Control-Allow-Methods"] = "GET, PUT, POST, DELETE, OPTIONS"
+    cherrypy.response.headers["Access-Control-Allow-Credentials"] = "true"
 
 if __name__ == '__main__':
-    
+    cherrypy.tools.CORS = cherrypy.Tool('before_finalize', CORS)
+    music = MusicWebService()
+    optionsController = Options()
+    dispatcher = cherrypy.dispatch.RoutesDispatcher()
+
+    #GET
+    dispatcher.connect('get_vals', '/',controller=music,action = 'GET',conditions=dict(method=['GET']))
+    dispatcher.connect('get_song', '/song/',controller=music,action = 'GET',conditions=dict(method=['GET']))
+
+    #PUT
+    dispatcher.connect('put_vals','/',controller=music,action = 'PUT',conditions=dict(method=['PUT']))
+    dispatcher.connect('put_song','/song/',controller=music,action = 'PUT',conditions=dict(method=['PUT']))
+
+
+    ###### OPTIONS #####
+    dispatcher.connect('options_root', '/', controller=optionsController, action = 'OPTIONS', conditions=dict(method=['OPTIONS']))
+    dispatcher.connect('options_song', '/song/', controller=optionsController, action = 'OPTIONS', conditions=dict(method=['OPTIONS']))
+
     conf = {
         '/': {
             'request.dispatch': cherrypy.dispatch.MethodDispatcher(),
             'tools.sessions.on': True,
             'tools.response_headers.on': True,
             'tools.response_headers.headers': [('Content-Type', 'text/plain')],
+            'tools.CORS.on': True
         }
     }
-    cherrypy.quickstart(MusicWebService(), '/', conf)
+    cherrypy.config.update(conf)
+    app = cherrypy.tree.mount(None, config=conf)
+    cherrypy.quickstart(app)
